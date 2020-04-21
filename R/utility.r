78463ecc7d9935e5974596232dbf96ca281193c3
XiaodanLyu <wuhandaxue2013@gmail.com>
2020-04-21 12:11:11 -0500

    Update utility.r

--- R/utility.r
+++ R/utility.r
@@ -1,6 +1,4 @@
 ## facility functions ####
-expo <- function(x) ifelse(x!=0, exp(x), 0)
-logo <- function(x) ifelse(x>0, log(x), 0)
 Gmat <- function(nvec){
   D <- length(nvec)
   arealab <- rep(1:D, times = nvec)
